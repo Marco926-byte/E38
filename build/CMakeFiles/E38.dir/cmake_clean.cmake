ce373879a26af054b2108c943481345070a56a69
Marco926-byte <malatesti.ma@gmail.com>
2024-08-30 15:23:01 +0200

    E38

---
+++ build/CMakeFiles/E38.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/E38.dir/main.cpp.o"
+  "CMakeFiles/E38.dir/main.cpp.o.d"
+  "E38"
+  "E38.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/E38.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
